c25524f2a63d3c5ee93dcdcf16828bca7d58c644
jackmez <jack@forpeople.com>
2020-07-06 18:14:30 +0200

    added export function

--- components/squareAnimation.js
+++ components/squareAnimation.js
@@ -9,7 +9,6 @@ export default class SquareAnimation extends React.Component {
 
   componentDidMount() {
 
-
     const scene = new THREE.Scene();
 
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
