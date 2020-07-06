55e6441b9a9738ee1c49b20048e46a1801d65fb7
jackmez <jack@forpeople.com>
2020-07-06 18:03:59 +0200

    install next.js and three.js animation

---
+++ components/squareAnimation.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import * as THREE from 'three';
+
+
+export default class SquareAnimation extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+
+
+    const scene = new THREE.Scene();
+
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    document.getElementById('canvas').appendChild( renderer.domElement );
+
+    var geometry = new THREE.BoxGeometry();
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    var animate = function () {
+      requestAnimationFrame( animate );
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render( scene, camera );
+    };
+
+    animate();
+
+  }
+
+render() {
+  return (
+    <section id="canvas" />
+  )
+}
+
+}
